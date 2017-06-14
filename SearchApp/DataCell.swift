83d11c3a3f2b4ed81c7c6b2eaf7699d1de3ae1d6
Kasey Schlaudt <kc@Kaseys-MacBook-Pro.local>
2017-06-14 09:56:09 -0600

    first commit

---
+++ SearchApp/DataCell.swift
@@ -0,0 +1,24 @@
+//
+//  DataCell.swift
+//  SearchApp
+//
+//  Created by Kasey Schlaudt on 6/14/17.
+//  Copyright © 2017 Kasey Schlaudt. All rights reserved.
+//
+
+import UIKit
+
+class DataCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
